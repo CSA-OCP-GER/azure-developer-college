f0a5b03bc4a55041b1cd6b470c8d64c91e47c19a
Andreas Mock <andreas.mock@live.com>
2019-11-11 18:59:51 +0100

    Added resource api

---
+++ day2/apps/dotnetcore/Scm.Resources/Adc.Scm.Resources.Api/Controllers/ContactImageController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Adc.Scm.Resources.Api.Repositories;
+using Adc.Scm.Resources.Api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Adc.Scm.Resources.Api.Controllers
+{
+    [ApiController]
+    [Route("contactimages")]
+    [Produces("application/json")]
+    public class ContactImageController : ControllerBase
+    {
+        private readonly ImageRepository _repository;
+        private readonly StorageQueueService _service;
+
+        public ContactImageController(ImageRepository repository, StorageQueueService service)
+        {
+            _repository = repository;
+            _service = service;
+        }
+
+        [HttpPost]
+        [ProducesResponseType((int)HttpStatusCode.Created)]
+        public async Task<IActionResult> UploadImage([FromForm(Name = "imageupload")]IFormFile file)
+        {
+            using (var memstream = new MemoryStream())
+            {
+                await file.CopyToAsync(memstream);
+
+                var name = await _repository.Add(file.FileName, memstream.ToArray());
+                await _service.NotifyImageCreated(name);
+
+                return CreatedAtAction(nameof(DownloadImage), new { image = name }, null);
+            }            
+        }
+
+        [HttpGet("{image}")]
+        [Produces("application/octet-stream")]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public async Task<IActionResult> DownloadImage(string image)
+        {
+            var data = await _repository.Get(image);
+            if (null == image)
+                return NotFound();
+
+            return File(data, "application/octet-stream");            
+        }
+    }
+}
