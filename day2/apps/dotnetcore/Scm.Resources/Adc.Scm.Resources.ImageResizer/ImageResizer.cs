9c18a6468e148c1a10a34daf873f176493e3b1fe
Andreas Mock <andreas.mock@live.com>
2019-11-13 20:02:07 +0100

    Added ImageResizer

--- day2/apps/dotnetcore/Scm.Resources/Adc.Scm.Resources.ImageResizer/ImageResizer.cs
+++ day2/apps/dotnetcore/Scm.Resources/Adc.Scm.Resources.ImageResizer/ImageResizer.cs
@@ -1,9 +1,9 @@
 using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp;
 
 namespace Adc.Scm.Resources.ImageResizer
 {
