1ba97bce9ff00da6dd7ba2f31c88a5631e78d35f
Andreas Mock <andreas.mock@live.com>
2019-11-15 10:52:07 +0100

    Contact AB#8 and thumbnails AB#23 (#20)
    * Added resize function
    
    * Added ImageResizer
    
    * Build pipelines resizer
    
    * FIX: added connectionstring to storage for funct
    
    * InputValidaten and container name
    
    * Added applicationInsights
    
    * Displayname
    
    * added missing image name
    
    * added missing resourcegroupname
    
    * FIX rg name for test
    
    * Added ApplicationInsights
    
    * Added binary upload
    
    * Added ConsumptionPlan
    
    * Added AvatarLocation
    
    * Upload copies raw image to thumbnails container
    
    * Added additional contact properties

--- day2/apps/dotnetcore/Scm/Adc.Scm.Api/Models/Contact.cs
+++ day2/apps/dotnetcore/Scm/Adc.Scm.Api/Models/Contact.cs
@@ -15,5 +15,13 @@ namespace Adc.Scm.Api.Models
         [Required]
         public string Company { get; set; }
         public string AvatarLocation { get; set; }
+        public string Phone { get; set; }
+        public string Mobile { get; set; }
+        public string Description { get; set; }
+        public string Street { get; set; }
+        public string HouseNumber { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
     }
 }
