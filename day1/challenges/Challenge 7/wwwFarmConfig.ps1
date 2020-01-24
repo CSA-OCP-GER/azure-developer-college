c9eeaccec470fbbcb2046bf69a0911c02643cebf
bfrank <bernhard_F@hotmail.com>
2020-01-24 14:32:43 +0100

    dscconfig

--- day1/challenges/Challenge 7/wwwFarmConfig.ps1
+++ day1/challenges/Challenge 7/wwwFarmConfig.ps1
@@ -5,16 +5,51 @@
     node ("localhost")
     {
         # Call Resource Provider
-        # E.g: WindowsFeature, File
+        # E.g: WindowsFeature
         WindowsFeature WWW {
+            Name   = "Web-Server"
             Ensure = "Present"
-            Name   = "Feature Name"
+        }
+
+        WindowsFeature ASPNet45 {
+            Name   = "Web-Asp-Net45"
+            Ensure = "Present"
+        }
+
+        WindowsFeature HTTPRedirection {
+            Name   = "Web-Http-Redirect"
+            Ensure = "Present"
+        }
+
+        WindowsFeature LogginTools {
+            Name   = "Web-Log-Libraries"
+            Ensure = "Present"
+        }
+
+        WindowsFeature RequestMonitor {
+            Name   = "Web-Request-Monitor"
+            Ensure = "Present"
+        }
+
+        WindowsFeature Tracing {
+            Name   = "Web-Http-Tracing"
+            Ensure = "Present"
+        }
+
+        WindowsFeature BasicAuthentication {
+            Name   = "Web-Basic-Auth"
+            Ensure = "Present"
+        }
+
+        WindowsFeature WindowsAuthentication {
+            Name   = "Web-Windows-Auth"
+            Ensure = "Present"
         }
 
         File DownloadPackage {            	
             Ensure          = "Present"              	
             Type            = "File"             	
-            SourcePath      = "https://storageaccount.blob.core.windows.net/mycontainer/web.zip"            	
+            SourcePath      = "https://github.com/CSA-OCP-GER/azure-developer-college/raw/features/day1handson/day1/challenges/Challenge%207/web.zip"            	
             DestinationPath = "C:\temp"   
   
         }
