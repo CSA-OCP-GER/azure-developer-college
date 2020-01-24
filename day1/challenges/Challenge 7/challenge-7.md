d5a8b23a2dec6afcc3dc804ab153ce1f901a932e
bfrank <bernhard_F@hotmail.com>
2020-01-24 14:21:17 +0100

    Challenge7

---
+++ day1/challenges/Challenge 7/challenge-7.md
@@ -0,0 +1,10 @@
+# Challenge 7: Networking: Loadbalancing your WWW Server Farm
+
+[back](../../readme.md)
+
+## Here is what you will learn ##
+
+- Create your first Azure resources using the portal
+- Learn how to navigate through the marketplace using search
+- Experience the wizard / templates for creating azure resources
+- See the unified deployment status page.
---
+++ day1/challenges/Challenge 7/wwwFarmConfig.ps1
@@ -0,0 +1,32 @@
+﻿configuration wwwFarmConfig
+{
+    # One can evaluate expressions to get the node list
+    # E.g: $AllNodes.Where("Role -eq Web").NodeName
+    node ("localhost")
+    {
+        # Call Resource Provider
+        # E.g: WindowsFeature, File
+        WindowsFeature WWW {
+            Ensure = "Present"
+            Name   = "Feature Name"
+        }
+
+        File DownloadPackage {            	
+            Ensure          = "Present"              	
+            Type            = "File"             	
+            SourcePath      = "https://storageaccount.blob.core.windows.net/mycontainer/web.zip"            	
+            DestinationPath = "C:\temp"   
+  
+        }
+        Archive WebArchive {
+            Destination = "c:\inetpub\wwwroot"
+            Path        = "c:\temp\web.zip"
+            DependsOn   = @("[File]DownloadPackage", "[WindowsFeature]WWW")
+         
+        }
+    }
+}
+
+
+
+
