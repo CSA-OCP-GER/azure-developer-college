e6ca416f4f709f86adc33a412a4cdb9e6e9ef0d8
Andreas Mock <andreas.mock@live.com>
2020-02-12 14:50:56 +0100

    fix

--- day5/apps/infrastructure/scripts/aad-integration.ps1
+++ day5/apps/infrastructure/scripts/aad-integration.ps1
@@ -9,8 +9,7 @@ param(
     [string]$UiAppReplyUrl)
     
     # read the exposed permission first
-    $exposedPermissionsFromJson = Get-Content -Raw -Path ./oauth2-permissions.json `
-        | ConvertFrom-Json
+    $exposedPermissionsFromJson = Get-Content -Raw -Path ./oauth2-permissions.json | ConvertFrom-Json
 
     Write-Host $exposedPermissionsFromJson
 
