d7183d1499b75b9c87b8f007410705479de7259b
Andreas Mock <andreas.mock@live.com>
2020-02-12 14:47:00 +0100

    fix script block

--- day5/apps/infrastructure/scripts/aad-integration.ps1
+++ day5/apps/infrastructure/scripts/aad-integration.ps1
@@ -12,19 +12,21 @@ param(
     $exposedPermissionsFromJson = Get-Content -Raw -Path ./oauth2-permissions.json |
         ConvertFrom-Json
 
+    Write-Host $exposedPermissionsFromJson
+
     $exposedPermissions = New-Object System.Collections.Generic.List[Microsoft.Open.AzureAD.Model.OAuth2Permission]
 
     $exposedPermissionsFromJson |
         ForEach-Object -Process {
             $permission = New-Object Microsoft.Open.AzureAD.Model.OAuth2Permission
             $permission.Id = [System.Guid]::Parse($_.id)
-            $permission.Origin = $_.Origin
+            $permission.Origin = $_.origin
             $permission.IsEnabled = $true
-            $permission.Type = $_.Type
-            $permission.AdminConsentDisplayName = $_.AdminConsentDisplayName
-            $permission.AdminConsentDescription = $_.AdminConsentDescription
-            $permission.UserConsentDescription = $_.UserConsentDescription
-            $permission.UserConsentDisplayName = $_.UserConsentDisplayName
+            $permission.Type = $_.type
+            $permission.AdminConsentDisplayName = $_.adminConsentDisplayName
+            $permission.AdminConsentDescription = $_.adminConsentDescription
+            $permission.UserConsentDescription = $_.userConsentDescription
+            $permission.UserConsentDisplayName = $_.userConsentDisplayName
             $permission.Value = $_.value
 
             $exposedPermissions.Add($permission)
