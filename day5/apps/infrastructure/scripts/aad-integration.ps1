9e35b2461f051b8e73ecd4ed50b70bfacd4e2436
Andreas Mock <andreas.mock@live.com>
2020-02-12 14:31:37 +0100

    Converted json permissions

--- day5/apps/infrastructure/scripts/aad-integration.ps1
+++ day5/apps/infrastructure/scripts/aad-integration.ps1
@@ -9,9 +9,28 @@ param(
     [string]$UiAppReplyUrl)
     
     # read the exposed permission first
-    $exposedPermissions = Get-Content -Raw -Path ./oauth2-permissions.json |
+    $exposedPermissionsFromJson = Get-Content -Raw -Path ./oauth2-permissions.json |
         ConvertFrom-Json
+
+    $exposedPermissions = New-Object System.Collections.GenericList.List[Microsoft.Open.AzureAD.Model.OAuth2Permission]
+
+    $exposedPermissionsFromJson |
+        ForEach-Object 
+        {
+            $permission = New-Object Microsoft.Open.AzureAD.Model.OAuth2Permission
+            $permission.Id = [System.Guid]::Parse($_.id)
+            $permission.Origin = $_.origin
+            $permission.IsEnabled = $true
+            $permission.Type = $_.Type
+            $permission.AdminConsentDisplayName = $_.adminConsentDisplayName
+            $permission.AdminConsentDescription = $_.AdminConsentDescription
+            $permission.UserConsentDescription = $_.userConsentDescription
+            $permission.UserConsentDisplayName = $_.userConsentDisplayName
+            $permission.Value = $_.value
+
+            $exposedPermissions.Add($permission)
+        }
     # create the ApiApp
-    $apiApp = New-AzureADApplication -DisplayName $ApiAppName -IdentifierUris $ApiAppUri -Oauth2Permissions $exposedPermissions
+    $apiApp = New-AzureADApplication -DisplayName $ApiAppName -IdentifierUris $ApiAppUri -Oauth2Permissions $exposedPermissions -AvailableToOtherTenants $false 
     New-AzureADServicePrincipal -AppId $apiApp.AppId
 
