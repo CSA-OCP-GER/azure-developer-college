55e228eb7bc3d509530dd7ee8bae9db38389ae82
Andreas Mock <andreas.mock@live.com>
2020-03-09 13:20:38 +0100

    started dapr

---
+++ dayX/apps/dotnetcore/Scm/Adc.Scm.Events/EventService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Options;
+using System.Threading.Tasks;
+using Dapr;
+
+namespace Adc.Scm.Events
+{
+    public class EventService
+    {
+        private readonly EventServiceOptions _options;
+        private readonly PublishClient _publishClient;
+
+        public EventService(IOptions<EventServiceOptions> options, PublishClient publishClient)
+        {
+            _options = options.Value;
+            _publishClient = publishClient;
+        }
+
+        public async Task Send(EventBase evt)
+        {
+            await _publishClient.PublishEventAsync(_options.TopicName, evt);
+        }
+    }
+}
